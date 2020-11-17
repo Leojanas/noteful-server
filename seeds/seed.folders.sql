4abe3ad7695e818fb98913eda0e26ded0341062f
Leo Janas <leojanas7@gmail.com>
2020-11-17 08:52:23 -0700

    Initial setup with db migrations and seed scripts

---
+++ seeds/seed.folders.sql
@@ -0,0 +1,6 @@
+INSERT INTO folders (folder_name, modified)
+VALUES (
+    ('People', now()),
+    ('Places', now()),
+    ('Things', now())
+)
---
+++ seeds/seed.notes.sql
@@ -0,0 +1,10 @@
+INSERT INTO notes (note_name, modified, content, folder_id)
+VALUES(
+    ('Charles', now(), 'My name is Charles.', 1),
+    ('Lucy', now(), 'My name is Lucy', 1),
+    ('Salt Lake City', now(), 'I am in Utah.', 2),
+    ('Denver', now(), 'I am in Colorado.', 2),
+    ('Austin', now(), 'I am in Texas.', 2),
+    ('Pot', now(), 'I am in the kitchen.', 3),
+    ('Couch', now(), 'I am in the living room.', 3)
+)
